65feb7ee10ded9c45e7b431b0432adfb574a25e8
saralindqvist <sarathereselindqvist@hotmail.com>
2022-05-24 14:52:58 +0200

    fixed error from conflicts (there was two files for "login")

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,167 +1,169 @@
 import {
-    Box,
-    Button,
-    Container,
-    TextField,
-    Typography,
-    Alert,
-    AlertProps
-  } from "@mui/material";
+  Box,
+  Button,
+  Container,
+  TextField,
+  Typography,
+  Alert,
+  AlertProps,
+} from "@mui/material";
 import { useState } from "react";
-  import { NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { makeRequest } from "../Helper";
 
 function MuiAlert(props: JSX.IntrinsicAttributes & AlertProps) {
-    return <Alert elevation={6} variant="filled" {...props} />;
-  }
-  
-  function LogIn() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
+  return <Alert elevation={6} variant="filled" {...props} />;
+}
 
-    const signInHandler = (e) => {
-        e.preventDefault();
-        if(email === '' || password === '') {
-            setError("All fields are required");
-            return;
-        }
-        setError('');
-        const userData = {
-            email: email,
-            password: password,
-            isAdmin: false,
-        }
+function LogIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-        makeRequest('/api/login', "POST", userData);
+  const signInHandler = (e) => {
+    e.preventDefault();
+    if (email === "" || password === "") {
+      setError("All fields are required");
+      return;
     }
+    setError("");
+    const userData = {
+      email: email,
+      password: password,
+      isAdmin: false,
+    };
 
-    return (
-      <Container
+    makeRequest("/api/login", "POST", userData);
+  };
+
+  return (
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        padding: "1rem",
+        minHeight: "35rem",
+        alignItems: "center",
+      }}
+    >
+      {error && (
+        <MuiAlert
+          style={{
+            marginBottom: "-2rem",
+            marginTop: "2rem",
+          }}
+          severity="error"
+          onClick={() => setError("")}
+        >
+          {error}
+        </MuiAlert>
+      )}
+      <Box
         sx={{
+          height: 510,
+          width: 400,
+          backgroundColor: "#F3F2F0",
           display: "flex",
           flexDirection: "column",
-          padding: "1rem",
-          minHeight: "35rem",
           alignItems: "center",
+          justifyContent: "center",
+          mt: "4rem",
+          mb: "4rem",
+          paddingBottom: "5rem",
         }}
       >
-        {error && (
-            <MuiAlert 
-                style={{
-                    marginBottom: '-2rem', 
-                    marginTop: '2rem'
-                }} 
-                severity="error" 
-                onClick={() => setError('')}>
-                {error}
-            </MuiAlert>
-        )}
         <Box
           sx={{
-            height: 510,
-            width: 400,
-            backgroundColor: "#F3F2F0",
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
-            justifyContent: "center",
-            mt: "4rem",
-            mb: "4rem",
-            paddingBottom: "5rem",
+            width: { xs: "300px", sm: "450px", md: "600px", lg: "600px" },
           }}
         >
+          <Typography
+            sx={{
+              textTransform: "uppercase",
+              fontFamily: "Prata",
+              alignItems: "center",
+              mt: "5.5rem",
+              mb: "1rem",
+            }}
+            variant="h5"
+          >
+            Log In
+          </Typography>
           <Box
             sx={{
+              borderTop: "1px solid #AAAAAA",
+              width: "50%",
+              margin: ".5rem auto",
+            }}
+          ></Box>
+          <form
+            style={{
               display: "flex",
               flexDirection: "column",
               alignItems: "center",
-              width: { xs: "300px", sm: "450px", md: "600px", lg: "600px" },
+              justifyContent: "center",
             }}
+            onSubmit={signInHandler}
           >
-            <Typography
-              sx={{
-                textTransform: "uppercase",
-                fontFamily: "Prata",
-                alignItems: "center",
-                mt: "5.5rem",
-                mb: "1rem",
+            <TextField
+              style={{
+                backgroundColor: "white",
+                height: "55px",
+                marginBottom: ".3rem",
+                width: "18rem",
               }}
-              variant="h5"
+              id="email"
+              name="email"
+              label="Email"
+              type="text"
+              margin="normal"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextField
+              style={{
+                backgroundColor: "white",
+                height: "55px",
+                marginBottom: ".3rem",
+                width: "18rem",
+              }}
+              id="password"
+              name="password"
+              label="Password"
+              type="password"
+              margin="normal"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button
+              variant="contained"
+              style={{
+                display: "flex",
+                width: "fit-content",
+                backgroundColor: "#CAC2B9",
+                textTransform: "none",
+                marginTop: "1rem",
+              }}
+              type="submit"
             >
               Log In
-            </Typography>
-            <Box
-              sx={{
-                borderTop: "1px solid #AAAAAA",
-                width: "50%",
-                margin: ".5rem auto",
-              }}
-            ></Box>
-            <form
-                style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    justifyContent: "center"
-                }}
-                onSubmit={signInHandler}>
-                <TextField
-                    style={{
-                        backgroundColor: "white",
-                        height: "55px",
-                        marginBottom: ".3rem",
-                        width: "18rem",
-                    }}
-                    id="email"
-                    name="email"
-                    label="Email"
-                    type="text"
-                    margin="normal"
-                    onChange={e => setEmail(e.target.value)}
-                />
-                <TextField
-                    style={{
-                        backgroundColor: "white",
-                        height: "55px",
-                        marginBottom: ".3rem",
-                        width: "18rem",
-                    }}
-                    id="password"
-                    name="password"
-                    label="Password"
-                    type="password"
-                    margin="normal"
-                    onChange={e => setPassword(e.target.value)}
-                />
-                <Button
-                    variant="contained"
-                    style={{
-                        display: "flex",
-                        width: "fit-content",
-                        backgroundColor: "#CAC2B9",
-                        textTransform: "none",
-                        marginTop: "1rem",
-                    }}
-                    type="submit"
-                >
-                Log In
-                </Button>
-            </form>
-            <NavLink
-              style={{
-                color: "#303030",
-                textDecoration: "none",
-                marginTop: "3rem",
-              }}
-              to="/signup"
-            >
-              Don't have an account? <b>Sign up</b>
-            </NavLink>
-          </Box>
+            </Button>
+          </form>
+          <NavLink
+            style={{
+              color: "#303030",
+              textDecoration: "none",
+              marginTop: "3rem",
+            }}
+            to="/signup"
+          >
+            Don't have an account? <b>Sign up</b>
+          </NavLink>
         </Box>
-      </Container>
-    );
-  }
-  
-  export default LogIn;
+      </Box>
+    </Container>
+  );
+}
+
+export default LogIn;
